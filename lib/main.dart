1752250c1be59431ff82236212c4aa31a2adde2a
Navaneeth.P <navaneethpalakkal285@gmail.com>
2024-11-19 11:14:41 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_8/niv.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false,home: ooo(),));
+}
